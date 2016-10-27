3909ccaee98c9684122a65ed297198d7e971a1dd
Sven Turowski <sventurowski@gmx.de>
2016-10-27 09:27:48 +0200

    update license

--- main.cpp
+++ main.cpp
@@ -5,8 +5,28 @@
 
     This file is part of cppsql, a C++ collection.
 
+    cppsql is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License (LGPL) as published
+    by the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    As a special exception, the Contributors give you permission to link
+    this library with independent modules to produce an executable,
+    regardless of the license terms of these independent modules, and to
+    copy and distribute the resulting executable under terms of your choice,
+    provided that you also meet, for each linked independent module, the
+    terms and conditions of the license of that module. An independent
+    module is a module which is not derived from or based on this library.
+    If you modify this library, you must extend this exception to your
+    version of the library.
+
     cppsql is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY;
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "src/querybuilder.h"
@@ -21,7 +41,9 @@ int main(int argc, char* argv[])
                 .select("BLOCKED", "C").select("NAME1", "P").select("NAME2", "P").select("TITLE", "P").select("SEX",
                         "P")
                 .select("DATE_OF_BIRTH", "P");
-        sqb.from("CUSTOMER", "C").from("PERSON", "P");
+        sqb.select("PHONE", "PH");
+        sqb.from("CUSTOMER", "C");
+        sqb.leftJoin(From("PERSON", "P", "PERSON_ID"), From("PHONE", "PH", "PERSON_ID"), Comparison::EQUALS);
         sqb.where("C.PERSON_ID = P.PERSON_ID", Operator::AND)
                 .where("C.CUSTOMER_ID like ?", Operator::AND)
                 .where("C.CLIENT = ?", Operator::AND)
